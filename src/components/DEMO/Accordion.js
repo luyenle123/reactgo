5423ace10784bd262870816f7b886e280ae5bd9c
NITECO\luyen.le <luyen.le@niteco.se>
2024-09-16 15:22:40 +0700

    test

--- src/components/DEMO/Accordion.js
+++ src/components/DEMO/Accordion.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 function Panel({title, children, isActive, onShow}){
     //const [isActive, setIsActive] = useState(false);
+    
     return(
         <section className='panel'>
             <h3>{title}</h3>
